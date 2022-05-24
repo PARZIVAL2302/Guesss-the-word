ff42d36a0e6130e8038048141f1452a6044eb15d
PARZIVAL2302 <91666412+PARZIVAL2302@users.noreply.github.com>
2022-05-24 18:07:54 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,37 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn- " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn- " + player2_name;
+
+function send(){
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log("Word in lower case = " + word);
+    charAt1 = word.charAt(1);
+    console.log(charAt1);
+    length_divide_2 = Math.floor(word.length/2);
+    charAt2 = word.charAt(length_divide_2);
+    console.log(charAt2);
+    length_minus_1 = word.length - 1;
+    charAt3 = word.charAt(length_minus_1);
+    console.log(charAt3);
+    remove_chartAt1 = word.replace(chartAt1, "_"); 
+    remove_chartAt3 = remove_chartAt2.replace(chartAt3, "_");
+    remove_chartAt2 = remove_chartAt1.replace(chartAt2, "_"); 
+    question_word = "<h4 id= 'word_display'> Q. " + remove_chartAt3 + "</h4>";
+    input_box = "<br> Answer: <input type='text' id='input_check_box'>";
+    check_button = "<br> <br> <button class='btn btn-info' onclick='check()'> Check </button>";
+    row = question_word + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value = "";
+}
